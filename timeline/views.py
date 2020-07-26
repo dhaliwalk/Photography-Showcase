2a78cbd753e1a8053ae44b87eb9b5daff3ff87ec
dhaliwalk <kevindhal@gmail.com>
2020-07-26 05:07:03 -0700

    added templates

--- timeline/views.py
+++ timeline/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+	{
+		'author': 'hi',
+		'title': 'blog post 1',
+		'content': 'content',
+		'date_posted': '2020'
+	},
+	{
+		'author': 'hi2',
+		'title': 'blog post 2',
+		'content': 'content2',
+		'date_posted': '2021'
+	}
+]
 def home(request):
-	return HttpResponse('<h1>Timeline Home</h1>')
+	context = {
+		'posts': posts,
+		'title': 'Timeline'
+	}
+	return render(request, 'timeline/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Timeline About</h1>')
+	return render(request, 'timeline/about.html', {'title': 'about'})
